8ac4400324977317e07cf6e454c9c7baabe0a66f
jhhim <jhhim99@gmail.com>
2023-05-13 19:09:45 +0900

    underbar

--- app/src/main/java/com/jhhim99/myapplication/MainActivity.java
+++ app/src/main/java/com/jhhim99/myapplication/MainActivity.java
@@ -1,50 +1,59 @@
 package com.jhhim99.myapplication;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
+
+
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
-import android.widget.LinearLayout;
-import android.widget.ScrollView;
+
+
+import com.google.android.material.navigation.NavigationBarView;
+
 
 public class MainActivity extends AppCompatActivity {
+    HomeFragment homeFrament;
+    ManageFragment manageFragment;
+    UserFragment userFragment;
 
-    CalendarView cldv;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        cldv = (CalendarView) findViewById(R.id.calenderView);
-    }
+        homeFrament = new HomeFragment();
+        manageFragment = new ManageFragment();
+        userFragment = new UserFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFrament).commit();
+
+        NavigationBarView navigationBarView = findViewById(R.id.bottom_navigationview);
+        navigationBarView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, homeFrament).commit();
+                        return true;
+                    case R.id.fork:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, manageFragment).commit();
+                        return true;
+                    case R.id.user:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.containers, userFragment).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+   }
 
     // 메뉴 리소스 XML의 내용을 앱바(App Bar)에 반영
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater ().inflate (R.menu.menu1, menu);
 
-        return true;
-    }
 
-    //앱바(App Bar)에 표시된 액션 또는 오버플로우 메뉴가 선택되면
-    //액티비티의 onOptionsItemSelected() 메서드가 호출
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId ()) {
-            case R.id.calendar:
-               cldv.setVisibility(android.view.View.VISIBLE);
-                return true;
-            default:
-                return super.onOptionsItemSelected (item);
-        }
-    }
 }
